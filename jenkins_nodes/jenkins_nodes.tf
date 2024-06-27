eba17b47e8194616833bcef3a610c170ba1392a3
Mykyta (Ban) <sam_banshee@pm.me>
2024-06-27 13:12:59 +0300

    working on jenkins and jenkins nodes

--- jenkins_nodes/jenkins_nodes.tf
+++ jenkins_nodes/jenkins_nodes.tf
@@ -1,5 +1,9 @@
+variable "jenkins_nodes" {
+  type = number
+}
+
 resource "virtualbox_vm" "node" {
-  count     = 2
+  count     = var.jenkins_nodes
   name      = format("node-%02d", count.index + 1)
   image     = "https://app.vagrantup.com/ubuntu/boxes/focal64/versions/20240612.0.1/providers/virtualbox/unknown/vagrant.box"
   cpus      = 2
